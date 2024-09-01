742291b12c026211ba80fb5aaa2c6e0963d49d31
KroNton <kyrlosfekry@gmail.com>
2024-09-01 16:57:11 +0300

    add Example: Basic Hello World Program

---
+++ 1-C++Fundamentals/src/ex1.cpp
@@ -0,0 +1,6 @@
+#include <iostream> // Include the input/output library
+
+int main() {
+    std::cout << "Hello, ROS World!" << std::endl; // Output text to the console
+    return 0; // Return a status code
+}
